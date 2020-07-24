75eac2093977c4dafc50af441e44cf3b0d60d810
Joshua <30259508@cityofglacol.ac.uk>
2020-07-24 14:02:51 +0100

    Refactored Search page functionality into own component

--- src/App.js
+++ src/App.js
@@ -1,50 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import * as BooksAPI from './BooksAPI'
 import Book from './Book'
+import SearchPage from './SearchPage'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 const BooksApp = () => {
   const [ showSearchPage, setShowSearchPage ] = useState(false)
+  const [ books, setBooks ] = useState([])
   
-  // state = {
-  //   /**
-  //    * TODO: Instead of using this state variable to keep track of which page
-  //    * we're on, use the URL in the browser's address bar. This will ensure that
-  //    * users can use the browser's back and forward buttons to navigate between
-  //    * pages, as well as provide a good URL they can bookmark and share.
-  //    */
-  //   showSearchPage: false
-  // }
+  useEffect(() => {
+    BooksAPI
+      .getAll()
+      .then(books => {
+        console.log('Books from hooks', books)
+        setBooks(books)
+      })
+  }, [])
 
   
   return (
     <div className="app">
       {showSearchPage ? (
-        <div className="search-books">
-          <div className="search-books-bar">
-            <button className="close-search" onClick={() => setShowSearchPage(false)}>Close</button>
-            <div className="search-books-input-wrapper">
-              {/*
-                NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                You can find these search terms here:
-                https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                you don't find a specific author or title. Every search is limited by search terms.
-              */}
-              <input type="text" placeholder="Search by title or author"/>
-
-            </div>
-          </div>
-          <div className="search-books-results">
-            <ol className="books-grid"></ol>
-          </div>
-        </div>
+        <SearchPage showSearchPage={() => setShowSearchPage()} />
       ) : (
         <div className="list-books">
           <div className="list-books-title">
             <h1>MyReads</h1>
           </div>
+        
           <div className="list-books-content">
             <div>
               <div className="bookshelf">
