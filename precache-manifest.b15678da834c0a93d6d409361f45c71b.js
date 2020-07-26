1c254464dc79861ac181ab5a397e9fb84ba4b485
Joshua <30259508@cityofglacol.ac.uk>
2020-07-26 20:07:25 +0100

    Updates

---
+++ precache-manifest.b15678da834c0a93d6d409361f45c71b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7012986b82cf5af4a49297b99f8b5193",
+    "url": "/my-reads-react-nanodegree/index.html"
+  },
+  {
+    "revision": "d1a8a7cc3b3d8d28e9c6",
+    "url": "/my-reads-react-nanodegree/static/css/main.8c5423b4.chunk.css"
+  },
+  {
+    "revision": "e38b44910e237a3e920d",
+    "url": "/my-reads-react-nanodegree/static/js/2.b651d4c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-reads-react-nanodegree/static/js/2.b651d4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a8a7cc3b3d8d28e9c6",
+    "url": "/my-reads-react-nanodegree/static/js/main.fe699beb.chunk.js"
+  },
+  {
+    "revision": "05ce3640f218341586b8",
+    "url": "/my-reads-react-nanodegree/static/js/runtime-main.d3c921d0.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/my-reads-react-nanodegree/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/my-reads-react-nanodegree/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/my-reads-react-nanodegree/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
